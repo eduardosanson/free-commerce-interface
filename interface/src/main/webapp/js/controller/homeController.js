1f0319011db5a3584bcdfdf9be375536a197d2ed
sanson <eduardo.sanson@m4u.com.br>
2015-12-21 21:35:33 -0200

    adicionando a logica para verificar se o preco atual e menor que anterior

--- interface/src/main/webapp/js/controller/homeController.js
+++ interface/src/main/webapp/js/controller/homeController.js
@@ -1,21 +1,22 @@
 app.controller('homeController',
     ['$scope','$http',function($scope,$http){
 
-        $http.get('http://localhost:8080/buscaController/quatroMaisVendidos').success(function(data){
+        $http.get('http://localhost:8090/buscaController/quatroMaisVendidos').success(function(data){
             $scope.products=data;
         });
 
-        $http.get('http://localhost:8080/buscaController/ultimosProdutosVendidos').success(function(data){
+        $http.get('http://localhost:8090/buscaController/ultimosProdutosVendidos').success(function(data){
             $scope.productLatest=data;
+            length = $scope.productLatest.length;
+            for(i=0;i<length;i++){
+                var product = $scope.productLatest[i];
+                if(product.lastPrice< product.price){
+                    product.lastPrice = null;
+                }
+            }
         });
 
-        for(var product in $scope.productLatest){
-            console.log(product.price)
-            if(product.price>product.lastPrice){
-                $scope.productLatest[$index].lastPrice=null;
 
-            }
-        }
 
 
    }]
